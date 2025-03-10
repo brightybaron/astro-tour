6877c2eef9222a9457d75490beaeca15fb08c2eb
brightybaron <ardi.panda54@protonmail.com>
2025-03-10 21:30:53 +0700

    edit height fasilitas

--- src/components/Fasilitas.tsx
+++ src/components/Fasilitas.tsx
@@ -9,7 +9,7 @@ const Fasilitas = ({ content }: any) => {
           <h2 className="text-2xl font-semibold">Fasilitas</h2>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 min-h-52">
           {content.fasilitas
             .slice(currentPage * 6, currentPage * 6 + 6)
             .map((inclusion: any) => (
